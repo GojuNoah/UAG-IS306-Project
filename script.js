79bf1e4783c5a3b554c65b01db1a9cb4d9f63bc9
Noah Stone <gojunoah@gmail.com>
2025-10-17 19:34:54 -0500

    test

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const calanderSection = document.getElementById('calanderSection');
+﻿"use strict"
 
-const date = new Date();
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 9
+   Coding Challenge 1
 
-// adding date to calander section
-calanderSection.innerHTML = `<p id=currentDate>${date}</p>`;
+   Clock
+   Author: Coding Challenge 1
+   Date:   2022-03-01
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+
+
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock() {
+
+   var thisDay = new Date();
+   var thisDate = thisDay.toLocaleDateString();
+   var thisDayNum = thisDay.getDay();
+   var thisWeekday = getWeekday(thisDayNum);
+   var thisTime = thisDay.toLocaleTimeString();
+   
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+}
+
+function getWeekday(dayNum) {
+   var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   return wDays[dayNum];
+}
