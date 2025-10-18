8f969be09e957ff25b62589886d36d745e084df6
Noah Stone <gojunoah@gmail.com>
2025-10-17 20:02:53 -0500

    added needed JS for course work and clock

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 ﻿"use strict"
 
-/*
-   New Perspectives on HTML5 and CSS3, 8th Edition
-   Tutorial 9
-   Coding Challenge 1
-
-   Clock
-   Author: Coding Challenge 1
-   Date:   2022-03-01
-
-   function getWeekday(dayNum)
-      Returns the text of the day of the week where dayNum
-      is the number of the week from 0 (Sunday) to 6 (Saturday)
-*/
-
-
 runClock();
 setInterval("runClock()", 1000);
 
@@ -31,6 +16,11 @@ function runClock() {
    document.getElementById("time").textContent = thisTime;
 }
 
+/*
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
 function getWeekday(dayNum) {
    var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
    return wDays[dayNum];
